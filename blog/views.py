69af009b95111c29e6e2e05af42755b0f7bd9323
Gabi Dombrowski <gabdombrowski@gmail.com>
2018-07-20 15:06:05 -0500

    Add post_detail url to post title

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,5 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    print('pk: ', pk, post)
     return render(request, 'blog/post_detail.html', {'post': post})
